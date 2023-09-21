935cef986ffe617101ec9f08439076a1b3016003
Shalu Chaddha <98653679+shalu2022@users.noreply.github.com>
2023-09-21 11:36:38 +0530

    Update index.js

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -25,7 +25,7 @@ export default function Footer() {
 	};
   
   useEffect(() => {
-      axios.get('https://hire-dashboard.vercel.app/api/v1/jobs').then((res)=>
+      axios.get('https://hire-dashboard.vercel.app/jobs').then((res)=>
        { setActiveJobData(res?.data.ActiveJobData);
         setCountReceived(res?.data?.ActiveJobData?.countPerDay?.Received)
         setCountApplied(res?.data?.ActiveJobData?.countPerDay?.Applied)
